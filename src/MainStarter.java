ce3b7482d71a91a61133e6c0ef17014e90ee2739
Stramor <malahart@mail.ru>
2013-04-04 19:33:20 +0400

    fadsfa

--- src/MainStarter.java
+++ src/MainStarter.java
@@ -6,9 +6,8 @@ public class MainStarter {
 
   public static void main(String[] args) throws IOException {
     List<Double> inputSignal = ReadingFiles.readFile("01.txt"); // загрузка исходного текстового файла
-    TryToProcess filteridze = new TryToProcess(); // создание объекта пользовательского класса
     BuildWindow.Drawing(BuildWindow.bindSeries(inputSignal, "WeAreTheChampions"),
-        BuildWindow.bindSeries(filteridze.filterSig(inputSignal), "New Window"));//Построение двух графиков.
+        BuildWindow.bindSeries(inputSignal, "New Window"));//Построение двух графиков.
     // 1 - исходный с названием WeAreTheChampions
     // 2 - список List обработанного сигнала. Аргумент этой функции - inputSignal - исходный сигнал. Назване графика New window
     System.out.println(Collections.max(inputSignal));// вывод на экран максимума списка inputSignal
